6e03a7e4411d62fb745846ffdd58d0d5ed43f5fe
daniir <danrodas03@gmail.com>
2022-02-22 23:36:54 -0500

    Frontend

---
+++ frontend/todo-list-app/src/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import FormTasks from "./components/FormTasks";
+import LayOut from "./components/Layout";
+import ListTasks from "./components/ListTasks";
+
+function App() {
+
+  //Agregar usuario para editar.
+  const [isUpdate, setIsUpdate] = React.useState(null);
+
+  return (
+    <div>
+      <LayOut>
+        <div className="container my-5">
+          <div className="row justify-content-center">
+            <div className="col">
+              <FormTasks isUpdate={isUpdate}/>
+            </div>
+            <div className="col">
+              <ListTasks setIsUpdate={setIsUpdate}/>
+            </div>
+          </div>
+        </div>
+      </LayOut>
+    </div>
+  );
+}
+
+export default App;
